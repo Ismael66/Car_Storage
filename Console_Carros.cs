96105d1664182c12f7576307ccd4a10f4bb033c0
Ismael Mateus <ismaelbnt66@gmail.com>
2021-12-08 19:17:09 -0300

    Switch Case base do menu principal

--- Console_Carros.cs
+++ Console_Carros.cs
@@ -20,6 +20,41 @@ namespace Car_Storage
                 "[5] Listar os veículos filtrando-se pelo modelo");
                 criaLinha();
                 Console.Write("Digite a opção desejada: ");
+                Escolha(Console.ReadLine());
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        static void Escolha(string? escolha)
+        {
+            try
+            {
+                Console.Clear();
+                criaLinha();
+                switch (escolha)
+                {
+                    case "1":
+                        Console.WriteLine("1");
+                        break;
+                    case "2":
+                        Console.WriteLine("2");
+                        break;
+                    case "3":
+                        Console.WriteLine("3");
+                        break;
+                    case "4":
+                        Console.WriteLine("4");
+                        break;
+                    case "5":
+                        Console.WriteLine("5");
+                        break;
+                    default:
+                        Console.WriteLine("default");
+                        Menu();
+                        break;
+                }
             }
             catch (System.Exception ex)
             {
